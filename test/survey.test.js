92f7bed679940135d32980d6d2ad500289a53e3c
psl012 <33899362+psl012@users.noreply.github.com>
2022-09-18 00:51:34 +0800

    Update survey.test.js

--- test/survey.test.js
+++ test/survey.test.js
@@ -6,7 +6,7 @@ const SurveyModel           = require('../models/survey.model');
 
 
 describe("Survey Model", function(){
-
+    /* Currently added so that we can trigger a new build for jenkis */
     it('Should return success message when input captcha matches.', function(){
         let surveyModel = new SurveyModel();
         let captcha = surveyModel.captcha;
